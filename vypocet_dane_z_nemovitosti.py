1cac2f1448226ad4031adf57a14bb3fdc121839f
Marketa Chvaja <meridian.czech@gmail.com>
2025-03-22 13:34:25 +0100

    druhá část úkolu - výpočet daně pro byt či dům

--- vypocet_dane_z_nemovitosti.py
+++ vypocet_dane_z_nemovitosti.py
@@ -22,7 +22,7 @@ class Property:
 
 # - land (zemědělský pozemek) má koeficient 0.85.
 # - building site (stavební pozemek) má koeficient 9.
-# - forrest (les) má koeficient 0.35,
+# - forest (les) má koeficient 0.35,
 # - garden (zahrada) má koeficient 2.      
 
 
@@ -37,21 +37,70 @@ class Estate(Property):
       tax = (self.area * 0.85 * self.locality.locality_coefficient)
     elif self.estate_type == "building_site":
       tax = (self.area * 9 * self.locality.locality_coefficient)
-    elif self.estate_type == "forrest":
+    elif self.estate_type == "forest":
       tax = (self.area * 0.35 * self.locality.locality_coefficient)
     elif self.estate_type == "garden":
       tax = (self.area * 2 * self.locality.locality_coefficient)
     else:
-      return "Chyba: Neplatný typ pozemku. Zadejte: land, building_site, forrest nebo garden."
+      return "Chyba: Neplatný typ pozemku. Zadejte: land, building_site, forest nebo garden."
     
     return ceil(tax)
 
 # Uvažujme tedy například lesní pozemek o ploše 500 metrů čtverečních v lokalitě s místním koeficientem 2. Potom je daň 500 * 0.35 * 2 = 350.
 
 
-zemedelsky_pozemek1 = Estate(locality_1, "forrest", 500)
-print(zemedelsky_pozemek1.calculate_tax())  # Mělo by vrátit 350
+zemedelsky_pozemek1 = Estate(locality_1, "forest", 500)
+print(zemedelsky_pozemek1.calculate_tax(), " Kč je daň za zemědělský pozemek č. 1.")  # Mělo by vrátit 350
 
-zemedelsky_pozemek2 = Estate(locality_3, "building_site", 25)
-print(zemedelsky_pozemek2.calculate_tax()) # Mělo by vrátit 788
+stavebni_pozemek2 = Estate(locality_3, "building_site", 25)
+print(stavebni_pozemek2.calculate_tax()) # Mělo by vrátit 788
 
+
+
+# Vytvoř třídu Residence`, která reprezentuje byt, dům či jinou stavbu a je potomkem třídy Property. Třída bude mít atributy locality, area (podlahová plocha bytu nebo domu) a commercial (pravdivostní hodnota, která určuje, zda se jedná o nemovitost používanou k podnikání). Dále přidej metodu calculate_tax(), která spočítá výši daně pro byt a vrátí hodnotu jako číslo. Daň vypočítej pomocí vzorce: podlahová plocha * koeficient lokality * 15. Pokud je hodnota parametru commercial True, tj. pokud jde o komerční nemovitost, vynásob celou daň číslem 2.
+
+class Residence(Property):
+  def __init__(self, locality: Locality, area, commercial):
+    super().__init__(locality)
+    self.area = area
+    self.commercial = commercial
+
+  def calculate_tax(self):
+    if self.commercial == True:
+      tax = self.area * self.locality.locality_coefficient * 15 * 2
+    elif self.commercial == False:
+      tax = self.area * self.locality.locality_coefficient * 15 
+    else:
+      return ("Parametr commercial musí být true nebo false.")
+    
+    return ceil(tax)
+  
+# Příklad výpočtu: Uvažujme tedy například byt (určený k bydlení) o ploše 60 metrů čtverečních v lokalitě s koeficientem 3. Potom je daň 60 * 3 * 15 = 2700. Pokud by stejný byt byl používán k podnikání, daň by byla 60 * 3 * 15 * 2 = 5400.
+
+byt1 = Residence(locality_2, 60, False)
+byt2 = Residence(locality_2, 60, True)
+
+print(byt1.calculate_tax()) # očekávaný výstup: 2700
+print(byt2.calculate_tax()) # očekávaný výstup: 5400
+
+
+# Vyzkoušej svůj program pomocí následujících nemovitostí:
+
+# - Zemědělský pozemek o ploše 900 metrů čtverečních v lokalitě Manětín s koeficientem 0.8. Daň z této nemovitosti je 900 * 0.85 * 0.8 = 612.
+
+locality_4 = Locality("Manětín", 0.8)
+zemedelsky_pozemek3 = Estate(locality_4, "land", 900)
+print(zemedelsky_pozemek3.calculate_tax()) # Mělo by vrátit 612
+
+
+# - Dům s podlahovou plochou 120 metrů čtverečních v lokalitě Manětín s koeficientem 0.8. Daň z této nemovitosti je 120 * 0.8 * 15 = 1440.
+
+rodinny_dum = Residence(locality_4, 120, False)
+print(rodinny_dum.calculate_tax()) # očekávaný výstup: 1440
+
+
+# - Kancelář (tj. komerční nemovitost) s podlahovou plochou 90 metrů čtverečních v lokalitě Brno s koeficientem 3. Daň z této nemovitosti je 90 * 3 * 15 * 2 = 8100.
+
+locality_5 = Locality("Brno", 3)
+kancelar = Residence(locality_5, 90, True)
+print(kancelar.calculate_tax()) # očekávaný výstup: 8100
